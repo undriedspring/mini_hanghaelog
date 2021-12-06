69de7d8cd2cb5c0f659de6c91fe8c52c01445d32
zubetcha <zuhye5@gmail.com>
2021-12-06 20:06:00 +0900

    Grid.js modified

--- src/elements/index.js
+++
@@ -1,8 +0,0 @@
-import Grid from "./Grid";
-import Image from "./Image"
-import Text from "./Text";
-import Input from "./Input";
-import Button from "./Button";
-import Spinner from "./Spinner";
-
-export {Grid, Image, Text, Input, Button, Spinner};
