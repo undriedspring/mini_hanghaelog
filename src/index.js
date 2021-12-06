4fba09b2aa78bc3c42bb9d3550237a31082f8670
undriedspring <hansam5615@gmail.com>
2021-12-06 21:36:18 +0900

    a

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
@@ -11,5 +10,3 @@ ReactDOM.render(<App />, document.getElementById('root'))
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
-=======
->>>>>>> zubetcha
