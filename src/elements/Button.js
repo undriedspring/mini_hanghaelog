2bb33e21525a48f0b4b0092e36d62e1c6db754ec
jawoon816 <jawoon816@gmail.com>
2021-12-06 20:34:53 +0900

    jww1

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -1,65 +1,34 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components"
 
-const Button = (props) => {
-  const { text, _onClick, is_float, children, margin, width, padding } = props;
+const Text =(props) => {
+    const {children, color, margin, size} = props;
 
-  if (is_float) {
-    return (
-      <React.Fragment>
-        <FloatButton onClick={_onClick}>{text? text : children}</FloatButton>
-      </React.Fragment>
-    );
-  }
+    const styles = {
+        color: color,
+        margin: margin,
+        size: size
+    }
+    return(
+        <React.Fragment>
+            <P {...styles}>{children}</P>
+        </React.Fragment>
+    )
+}
 
-  const styles = {
-    margin: margin,
-    width: width,
-    padding: padding,
-  };
+Text.defaultProps = {
+    children: null,
+    color: '#222831',
+    margin: false,
+    size: false,
 
-  return (
-    <React.Fragment>
-      <ElButton {...styles} onClick={_onClick}>{text? text: children}</ElButton>
-    </React.Fragment>
-  );
-};
+}
 
-Button.defaultProps = {
-  text: false,
-  children: null,
-  _onClick: () => {},
-  is_float: false,
-  margin: false,
-  width: "100%",
-  padding: "12px 0px",
-};
+const P = styled.p`
+    color: ${(props) => props.color};
+    font-size: ${(props) => props.size};
+    ${(props) => props.margin ? `${props.margin};` : ''}
+`   
 
-const ElButton = styled.button`
-  width: ${(props) => props.width};
-  background-color: #212121;
-  color: #ffffff;
-  padding: ${(props) => props.padding};
-  box-sizing: border-box;
-  border: none;
-  ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
-`;
 
-const FloatButton = styled.button`
-  width: 50px;
-  height: 50px;
-  background-color: #212121;
-  color: #ffffff;
-  box-sizing: border-box;
-  font-size: 36px;
-  font-weight: 800;
-  position: fixed;
-  bottom: 50px;
-  right: 16px;
-  text-align: center;
-  vertical-align: middle;
-  border: none;
-  border-radius: 50px;
-`;
-
-export default Button;
+export default Text;
--- src/elements/Text.js
+++ src/elements/Text.js
@@ -1,30 +1,34 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components"
 
-const Text = (props) => {
-  const { bold, color, size, children, margin } = props;
+const Text =(props) => {
+    const {children, color, margin, size} = props;
 
-  const styles = {bold: bold, color: color, size: size, margin};
-  return (
-      <P {...styles}>
-          {children}
-      </P>
-  )
-};
+    const styles = {
+        color: color,
+        margin: margin,
+        size: size
+    }
+    return(
+        <React.Fragment>
+            <P {...styles}>{children}</P>
+        </React.Fragment>
+    )
+}
 
 Text.defaultProps = {
-  children: null,
-  bold: false,
-  color: "#222831",
-  size: "14px",
-  margin: false,
-};
+    children: null,
+    color: '#222831',
+    margin: false,
+    size: false,
+
+}
 
 const P = styled.p`
-  color: ${(props) => props.color};
-  font-size: ${(props) => props.size};
-  font-weight: ${(props) => (props.bold? "600" : "400")};
-  ${(props) => (props.margin? `margin: ${props.margin};` : '')}
-`;
+    color: ${(props) => props.color};
+    font-size: ${(props) => props.size};
+    ${(props) => props.margin ? `${props.margin};` : ''}
+`   
+
 
 export default Text;
