fcee2f8a4fbe6f7ba1f2c4c74eb4d811179625a0
undriedspring <hansam5615@gmail.com>
2021-12-08 03:04:06 +0900

    json-server add

---
+++ src/redux/modules/comment.js
@@ -0,0 +1,43 @@
+import { createAction, handleActions } from 'redux-actions'
+import { produce } from 'immer'
+import { actionCreators as postActions } from './post'
+import 'moment'
+import moment from 'moment'
+
+const SET_COMMENT = 'SET_COMMENT'
+const ADD_COMMENT = 'ADD_COMMENT'
+const LOADING = 'LOADING'
+
+const setComment = createAction(SET_COMMENT, (post_id, comment_list) => ({ post_id, comment_list }))
+const addComment = createAction(ADD_COMMENT, (post_id, comment) => ({ post_id, comment }))
+const loading = createAction(LOADING, (is_loading) => ({ is_loading }))
+
+const initialState = {
+  list: {},
+  is_loading: false,
+}
+
+export default handleActions(
+  {
+    [SET_COMMENT]: (state, action) =>
+      produce(state, (draft) => {
+        draft.list[action.payload.post_id] = action.payload.comment_list
+      }),
+    [ADD_COMMENT]: (state, action) =>
+      produce(state, (draft) => {
+        draft.list[action.payload.post_id].unshift(action.payload.comment)
+      }),
+    [LOADING]: (state, action) =>
+      produce(state, (draft) => {
+        draft.is_loading = action.payload.is_loading
+      }),
+  },
+  initialState
+)
+
+const actionCreators = {
+  setComment,
+  addComment,
+}
+
+export { actionCreators }
