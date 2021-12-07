9a99ae39fc148a364e758d84ccf0e5b633d76625
undriedspring <hansam5615@gmail.com>
2021-12-08 00:49:52 +0900

    comments.js 작업

--- src/components/QuickMenu.js
+++ src/components/QuickMenu.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import './QuickMenu.css'
 import { Grid, Button, Image, Input, Text } from '../elements'
+import { history } from '../redux/configureStore'
+import Comments from '../pages/Comments'
 import AddCircleIcon from '@mui/icons-material/AddCircle'
 import ArrowDropDownCircleSharpIcon from '@mui/icons-material/ArrowDropDownCircleSharp'
 
@@ -8,8 +11,18 @@ const QuickMenu = () => {
   return (
     <React.Fragment>
       <Grid is_grid position="fixed" left="1050px" bottom="50px">
-        <AddCircleIcon class="AddButton" width="70px" cursor="pointer"></AddCircleIcon>
-        <ArrowDropDownCircleSharpIcon class="TopToButton" width="70px" cursor="pointer"></ArrowDropDownCircleSharpIcon>
+        <Grid position="relative"></Grid>
+        <AddCircleIcon
+          _onClick={() => {
+            history.push(`/api/posts/:id/comments`)
+          }}
+          class="AddButton"
+          width="70px"
+          cursor="pointer"
+        ></AddCircleIcon>
+        <div className="Rotate">
+          <ArrowDropDownCircleSharpIcon class="TopToButton" width="70px" cursor="pointer"></ArrowDropDownCircleSharpIcon>
+        </div>
       </Grid>
     </React.Fragment>
   )
