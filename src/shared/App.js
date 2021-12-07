d11c8f9cb9e36d0b6f82fa010671d108ea59a9f6
zubetcha <zuhye5@gmail.com>
2021-12-07 17:01:49 +0900

    App.js modified as origin-repo main branch

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,27 +1,15 @@
 import React from 'react'
-import styled from 'styled-components'
-
-import Post from './Post'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import { Grid } from '../elements/index'
+import { Grid } from '../elements'
+import QuickMenu from '../components/QuickMenu'
+import Comments from '../pages/Comments'
 
 function App() {
   return (
     <>
-      <div>
-        <Header />
-        <Grid is_flex alignContent="flex-start" position="relative" maxWidth="1000px" minWidth="650px" margin="130px auto">
-          <Grid maxWidth="300px" minWidth="150px" height="500px" bg="lightgrey" position="relative" top="120px">
-            <Footer />
-          </Grid>
-          <Grid width="100%" maxWidth="700px" minWidth="500px">
-            <Post></Post>
-            <Post></Post>
-            <Post></Post>
-          </Grid>
-        </Grid>
-      </div>
+      <Grid>
+        <Comments></Comments>
+        <QuickMenu></QuickMenu>
+      </Grid>
     </>
   )
 }
