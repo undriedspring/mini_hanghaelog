151da22706df218e37766688dab1d7ad965469c1
zubetcha <zuhye5@gmail.com>
2021-12-08 21:37:48 +0900

    PostWrite.js, api.js modified & post.js written

---
+++ src/redux/modules/post.js
@@ -0,0 +1,21 @@
+import { createAction, handleActions } from 'redux-actions'
+import { produce } from 'immer'
+
+// ************ modules ************ //
+import { actionCreators as imageActions } from './image'
+
+// ************ Action Type ************ //
+
+const GET_POST = 'GET_POST'
+const ADD_POST = 'ADD_POST'
+const EDIT_POST = 'EDIT_POST'
+const DELETE_POST = 'DELETE_POST'
+const LOADING = 'LOADING'
+
+// ************ Action Creator ************ //
+
+const getPost = createAction(GET_POST, (post_list, paging) => ({ post_list, paging }))
+const addPost = createAction(ADD_POST, (post) => ({ post }))
+const editPost = createAction(EDIT_POST, (postId, post) => ({ postId, post }))
+const deletePost = createAction(DELETE_POST, (postId) => ({ postId }))
+const loading = createAction(LOADING, (is_loading) => ({ is_loading }))
