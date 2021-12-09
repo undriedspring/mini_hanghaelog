44e543966d36cec3a94cc3afd0b1b7a2918d235a
jawoon816 <jawoon816@gmail.com>
2021-12-09 21:17:22 +0900

    user module거의완성

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,12 +1,11 @@
 import { createAction, handleActions } from 'redux-actions'
 import { produce } from 'immer'
 
-import { setCookie, getCookie, deleteCookie } from '../../shared/Cookie'
+import { setCookie, deleteCookie } from '../../shared/Cookie'
 import { applyMiddleware } from 'redux'
 
 import { apis } from '../../shared/fake_api'
 
-const LOG_IN = 'LOG_IN'
 const LOG_OUT = 'LOG_OUT'
 const GET_USER = 'GET_USER'
 const SET_USER = 'SET_USER'
@@ -21,12 +20,12 @@ const initialState = {
   is_login: false,
 }
 
-const user_initial = {
-  email: 'test@tt.tt',
-  nickname: 'test',
-  password: 'tttt12345*',
-  passwordcheck: 'tttt12345*',
-}
+// const user_initial = {
+//   email: 'test@tt.tt',
+//   nickname: 'test',
+//   password: 'tttt12345*',
+//   passwordcheck: 'tttt12345*',
+// }
 
 //middleware
 
@@ -38,7 +37,7 @@ const registerDB = (email, nickname, password, passwordCheck) => {
         //완료되면 res가 넘어오고
         console.log(res)
         history.push('/login')
-        window.alert('회원가입을 축하드립니다! 로그인 후 이용하실 수 있어요(о´∀`о)')
+        window.alert('회원가입을 축하드립니다! 로그인 후 이용하실 수 있어요╰(*´︶`*)╯♡')
       })
       .catch((err) => {
         //오류나면 이리로
@@ -54,7 +53,7 @@ const logInDB = (email, password) => {
       .then((res) => {
         console.log(res)
         setCookie('token', res.data[0].token, 3)
-        // localStorage.setItem()
+        localStorage.setItem('userID', res.data[0].userId)
         dispatch(setUser({ email: email }))
         history.replace('/posts')
       })
@@ -68,14 +67,14 @@ const logInDB = (email, password) => {
 const logOutDB = () => {
   return function (dispatch, getState, { history }) {
     deleteCookie('token')
-    localStorage.removeItem()
+    localStorage.removeItem('userID')
     dispatch(logOut('/login'))
   }
 }
 
 const loginCheckDB = () => {
   return function (dispatch, getState, { history }) {
-    const userId = localStorage.getItem('username')
+    const userId = localStorage.getItem('userID')
     const tokenCheck = document.cookie
     if (tokenCheck) {
       dispatch(SET_USER({ id: userId }))
@@ -90,13 +89,11 @@ export default handleActions(
   {
     [SET_USER]: (state, action) =>
       produce(state, (draft) => {
-        // setCookie('is_login', 'success')
         draft.user = action.payload.user
         draft.is_login = true
       }),
     [LOG_OUT]: (state, action) =>
       produce(state, (draft) => {
-        // deleteCookie('is_login')
         draft.user = null
         draft.is_login = false
       }),
